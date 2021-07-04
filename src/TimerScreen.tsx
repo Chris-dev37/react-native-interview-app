53beaeb582dafa17f38008d99a7c17392cdb53c2
c23-day <christopher4.day@live.uwe.ac.uk>
2021-07-04 21:04:42 +0100

    Timestamp if statement still not working for weather, but rest of funtionality ready for when it does

--- src/TimerScreen.tsx
+++ src/TimerScreen.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
 
-const TimerScreen = () => (
-  <View style={{ flex: 1, backgroundColor: '#ff4081' }}>
-    <Text>Page 1</Text>
-    <Button title="Press me"/>
-  </View>
-);
+const TimerScreen = () => {
+
+  const [count, setCount] = useState(10);
+  const [disable, setDisable] = useState(false);
+
+  const startCount = () => {
+    setDisable(true);
+  };
+  
+  useEffect(() => {
+    if (count > 0 && disable) {
+      setTimeout(() => setCount(count - 1), 1000);
+    } else {
+      setCount(10);
+      setDisable(false);
+    }
+
+  });
+  return (
+    <View style={{ flex: 1, backgroundColor: '#ff4081' }}>
+      <Text>Page 1</Text>
+      <Button title="Press me" onPress={startCount} disabled={disable} />
+      <Text style={{textAlign: 'center', fontSize: 50}}>{count}</Text>
+    </View>
+  )
+};
 
 export default TimerScreen;
--- src/WeatherScreen.tsx
+++ src/WeatherScreen.tsx
@@ -1,12 +1,63 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
-const WeatherScreen = () => (
-  <View style={{ flex: 1, backgroundColor: '#ff4081' }}>
-    <Text>Page 2</Text>
-    <Text>Temperature this week will be ...</Text>
-    <Text>19c, 21c, 23c ...</Text>
-  </View>
-);
+const WeatherScreen = () => {
+
+  fetch("http://api.openweathermap.org/data/2.5/forecast?zip=GL51,GB&units=metric&appid=12dd7ca9ea3ee83b8905ec822844bcd1")
+    .then(response => response.json())
+    .then((data) => {
+      compareTime(data);
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
+  let weatherObj = [];
+  const compareTime = (data) => {
+
+    let i = 0;
+    while (i < data["list"].length) {
+
+      let compareDate = new Date();
+      compareDate.setDate(compareDate.getDate() + i);
+      compareDate.setHours(10, 0, 0);
+      compareDate.setMilliseconds(0);
+      //console.log(compareDate.getTime());
+
+      let date = new Date(data["list"][i].dt * 1000)
+      // console.log(date);
+      // console.log(date);
+
+      let dayOfWeek = new Date(data["list"][i].dt * 1000).toLocaleDateString("en-UK", { weekday: "long", day: "numeric" });
+      //console.log(dayOfWeek);
+
+      if (date.getTime() === compareDate.getTime()) {
+        console.log(date);
+        console.log(data["list"][i]["main"]["temp"]);
+
+        weatherObj.push({
+          date: dayOfWeek,
+          temp: data["list"][i]["main"]["temp"],
+          icon: data["list"][i]["weather"][0].icon
+        });
+
+      }
+      i++;
+      //console.log(weatherObj);
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#ff4081' }}>
+      <Text>Page 2</Text>
+      <Text>Temperature this week will be ...</Text>
+      <ul>
+        {/* {weatherObj.forEach(e => {
+          <li>{e.date}: {e.temp} <img src={"http://openweathermap.org/img/w/" + {e.icon} + ".png"} alt="Weather icon"/></li>
+        })} */}
+      </ul>
+    </View>
+  )
+};
 
 export default WeatherScreen;
